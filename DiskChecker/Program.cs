3a6efa46744babba95a0b3294927532b2709ef52
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2024-02-17 18:35:53 +0100

    feat: add disk letter args and args verification

--- DiskChecker/Program.cs
+++ DiskChecker/Program.cs
@@ -3,8 +3,15 @@ internal static class Program
 {
     private static void Main(string[] args)
     {
-        var nSeconds = args.Length > 0 ? int.Parse(args[0]) : 5;
-
-		_ = new DiskChecker(nSeconds);
+        if (args.Length > 0 && args.Length < 2)
+        {
+            var diskLetter = args[0];
+            var nSeconds = int.Parse(args[1]);
+            
+            _ = new DiskChecker(diskLetter, nSeconds);
+        } else
+        {
+            throw new ArgumentException("Usage: DiskChecker <disk letter> <n seconds>");
+        }
     }
 }
