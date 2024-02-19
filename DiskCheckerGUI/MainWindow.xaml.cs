fe9e6e1c6e66791ad8e1ae8ab15732f8202b3c4a
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2024-02-19 12:39:31 +0100

    feat: select and run (crash)

--- DiskCheckerGUI/MainWindow.xaml.cs
+++ DiskCheckerGUI/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using Microsoft.Win32;
+using static DiskChecker.DiskChecker;
 
 namespace DiskCheckerGUI;
 
@@ -10,15 +9,42 @@ namespace DiskCheckerGUI;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private ReaderWriterLockSlim LogFileLock { get; } = new ReaderWriterLockSlim();
+    
     public MainWindow()
     {
         InitializeComponent();
         
+        foreach (var drive in DriveInfo.GetDrives())
+        {
+            if (drive.IsReady)
+            {
+                DriveListBox.Items.Add(drive.Name);
+            }
+        }
+    }
+
+    private void RunDiskChecker()
+    {
+        var diskLetter = DriveListBox.SelectedItem.ToString()?.ToUpper() ?? throw new InvalidOperationException();
+        var nSeconds = int.Parse(SecondsTextBox.Text);
+        
+
+        var diskChecker = new DiskChecker.DiskChecker(diskLetter, nSeconds);
+    }
+    
+    private void ReadLogFile()
+    {
         var userEnv = Environment.SpecialFolder.UserProfile;
         var userFolder = Environment.GetFolderPath(userEnv);
         var logPath = Path.Combine(userFolder, "DiskChecker", "DiskChecker.log");
         
         using var reader = new StreamReader(logPath);
         LogFileTextBox.Text = File.Exists(logPath) ? reader.ReadToEnd() : "No log file found";
+    }
+
+    private void RunButton_Click(object sender, RoutedEventArgs e)
+    {
+        RunDiskChecker();
     }
 }
